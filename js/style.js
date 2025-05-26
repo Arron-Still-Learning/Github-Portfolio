50b1802c6fe971660b14522b4d1ceef921d5613d
Hein Latt Naing <heinlattnaing.it@gmail.com>
2025-05-27 01:12:27 +0630

    live bg for banner

---
+++ js/style.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function () {
+  VANTA.GLOBE({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    size: 0.8
+  });
+});
